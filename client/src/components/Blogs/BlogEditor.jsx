5224fe9c2878b08bb8551968851e3c24a18c18ca
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-01-05 01:15:24 +0530

    Blog Editor Updated

--- client/src/components/Blogs/BlogEditor.jsx
+++ client/src/components/Blogs/BlogEditor.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, useEffect } from 'react'
-import { Link } from 'react-router-dom';
-// import defaultBanner from "../imgs/blog banner.png"
-import { EditorContext } from './Editor';
+import React, { useContext, useEffect } from "react"
+import { Link } from "react-router-dom";
+import defaultBanner from "../../assets/images/Blogs/default_banner.png"
+
+import { EditorContext } from "./Editor";
 import EditorJS from "@editorjs/editorjs";
-import { tools } from './BlogTools';
+import { tools } from "./BlogTools";
 import {Toaster,toast} from "react-hot-toast";
 
 const BlogEditor = () => {
@@ -31,7 +32,7 @@ const BlogEditor = () => {
   const handleTitleChange = (e) =>{
     let input = e.target;
 
-    input.style.height = 'auto';
+    input.style.height = "auto";
     input.style.height = input.scrollHeight + "px";
 
     setBlog({...blog,title:input.value})
@@ -61,24 +62,24 @@ const BlogEditor = () => {
   }
   return (
     <>
-      <nav className='navbar'>
+      <nav className="navbar">
       <Toaster />
-        <Link to="/" className='flex-none w-10'>
+        <Link to="/" className="flex-none w-10">
           {/* <img src={logo} alt="" /> */}
         </Link>
-        <p className='line-clamp-1 w-full'>
+        <p className="line-clamp-1 w-full">
           {title.length ? title : "New Blog"}
         </p>
-        <div className='flex gap-4 ml-auto'>
-          <button className='btn-dark py-2' onClick={handlePublishEvent}>Publish</button>
-          <button className='btn-light py-2'>Save Draft</button>
+        <div className="flex gap-4 ml-auto">
+          <button className="btn-purple py-2" onClick={handlePublishEvent}>Publish</button>
+          <button className="btn-purple py-2">Save Draft</button>
         </div>
       </nav>
       <section>
-        <div className='mx-auto max-w-[900px] w-full'>
-          <div className='relative aspect-video hover:opacity-80 bg-white border-4 border-grey'>
-              <label htmlFor='uploadBanner'>
-              {/* <img src={defaultBanner} className="z-20"/> */}
+        <div className="mx-auto max-w-[900px] w-full">
+          <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
+              <label htmlFor="uploadBanner">
+                  <img src={defaultBanner} className="z-20" alt="default blog banner"/>
                   <input
                     id="uploadBanner"
                     type="file"
@@ -90,15 +91,15 @@ const BlogEditor = () => {
           </div>
           <textarea
             defaultValue={title}
-            placeholder='blog-title'
-            className='w-full h-20 text-4xl font-medium outline-none resize-none mt-10 leading-tight placeholder:opacity-40'
+            placeholder="blog-title"
+            className="w-full h-20 text-4xl font-medium outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
             onKeyDown={handleTitleKeyDown}
             onChange={handleTitleChange}
           ></textarea>
 
-          <hr className='w-full opacity-10 my-5'/>
+          <hr className="w-full opacity-10 mb-5"/>
 
-          <div id='textEditor' className='font-gelasio'>
+          <div id="textEditor" className="font-gelasio px-0 mx-0">
 
           </div>
         </div>
