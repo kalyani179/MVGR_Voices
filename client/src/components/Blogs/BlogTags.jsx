a4a54b619cb4243d040ec04ac732812d9188187d
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-01-08 00:13:07 +0530

    updated BlogTags

--- client/src/components/Blogs/BlogTags.jsx
+++ client/src/components/Blogs/BlogTags.jsx
@@ -26,7 +26,7 @@ const BlogTags = ({tag,tagIndex}) => {
             <div className="relative p-2 mt-2 mr-2 px-5 bg-white rounded-full inline-block hover:bg-opacity-50 pr-10">
                 <p className="outline-none" onKeyDown={handleTagEdit} onClick={addEditable}>{tag}</p>
                 <button className="mt-[2px] rounded-full absolute right-3 top-1/2 -translate-y-1/2" onClick={handleTagDelete}>
-                    <i className="fi fi-br-cross text-xs pointer-events-none"></i>
+                    <i className="fi fi-br-cross text-sm pointer-events-none"></i>
                 </button>
             </div>
     )
