a9ce1d8f7d5c183582e55919cda2265222c2667a
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-02-10 14:26:31 +0530

    Added Likes,Comments to the BlogPage

--- client/src/components/Blogs/BlogPage/BlogPage.jsx
+++ client/src/components/Blogs/BlogPage/BlogPage.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Animation from '../../../common/Animation';
 import Loader from '../../../common/Loader';
@@ -16,6 +16,9 @@ export const blogStructure = {
     banner:'',
     publishedAt:''
 }
+
+export const BlogContext = createContext({});
+
 const BlogPage = () => {
     let {blog_id} = useParams();
     const [blog,setBlog] = useState(blogStructure);
@@ -39,25 +42,27 @@ const BlogPage = () => {
         <Animation>
             {
                 loading ? <Loader /> :
-                <div className="max-w-[900px] mx-auto block py-10 px-[5vw]">
-                    <img src={defaultBanner} className="aspect-video" alt="banner"/>
-                    <div className="mt-12">
-                        <h2>{title}</h2>
-                        <div className="flex sm:flex-col justify-between my-8">
-                            <div className="flex gap-5 items-start">
-                                <img src={profile_img} className="w-12 h-12 rounded-full" alt="profile" />
-                                <p className="capitalize">
-                                    {fullname}
-                                    <br />
-                                    @
-                                    <Link to={`/user/${author_username}`} className="underline">{author_username}</Link>
-                                </p>
+                <BlogContext.Provider value={{blog,setBlog}}>
+                    <div className="max-w-[900px] mx-auto block py-10 px-[5vw]">
+                        <img src={defaultBanner} className="aspect-video" alt="banner"/>
+                        <div className="mt-12">
+                            <h2>{title}</h2>
+                            <div className="flex sm:flex-col justify-between my-8">
+                                <div className="flex gap-5 items-start">
+                                    <img src={profile_img} className="w-12 h-12 rounded-full" alt="profile" />
+                                    <p className="capitalize">
+                                        {fullname}
+                                        <br />
+                                        @
+                                        <Link to={`/user/${author_username}`} className="underline">{author_username}</Link>
+                                    </p>
+                                </div>
+                                <p className="text-dark-grey opacity-75 sm:mt-6 sm:ml-12 sm:pl-5">published On {getFullDate(publishedAt)}</p>
                             </div>
-                            <p className="text-dark-grey opacity-75 sm:mt-6 sm:ml-12 sm:pl-5">published On {getFullDate(publishedAt)}</p>
                         </div>
+                        <BlogInteraction />
                     </div>
-                    <BlogInteraction />
-                </div>
+                </BlogContext.Provider>
             }
         </Animation>
     )
