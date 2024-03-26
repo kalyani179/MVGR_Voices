c6273b7933569bc5f2b1496780d93f101085098b
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-03-26 23:11:03 +0530

    UserProfile Page Updated missing changes

--- client/src/common/NoDataMessage.jsx
+++ client/src/common/NoDataMessage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NoDataMessage = ({message}) => {
     return (
-        <div className="">
+        <div>
             <p className="text-center text-primary text-lg font-medium p-4 px-20 rounded-full bg-grey/10 mt-4">{message}</p>
         </div>
     )
