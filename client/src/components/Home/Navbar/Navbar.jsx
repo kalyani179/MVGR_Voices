09ea836cb48452de7690471f0fa10c5411365267
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-03-26 17:55:09 +0530

    Added Notifications to Navbar, Handled Search Functionality

--- client/src/components/Home/Navbar/Navbar.jsx
+++ client/src/components/Home/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import UserAuth from "../../Authentication/UserAuth";
 import { ThemeContext, UserContext } from "../../../App";
 import { removeFromSession, storeInSession } from "../../../common/session";
@@ -9,26 +9,49 @@ import logoDark from "../../../assets/icons/logoDark.png";
 import { Link, useNavigate } from "react-router-dom";
 import UserNavigationPanel from "./UserNavigationPanel";
 import Footer from "../Footer/Footer";
+import axios from "axios";
 
 
 const Navbar = ({home=1,activeLink="Home"}) => {
   let navigate = useNavigate();
-  const {userAuth:{profile_img}} = useContext(UserContext);
   const [userNavPanel,setUserNavPanel] = useState(false);
   let {theme,setTheme} = useContext(ThemeContext);
-  const {userAuth:{access_token},setUserAuth} = useContext(UserContext);
+  const {userAuth,userAuth:{access_token,profile_img,new_notification_available},setUserAuth} = useContext(UserContext);
 
   const [showSignup,setShowSignup] = useState(false);
   const [showSignin,setShowSignin] = useState(false);
 
 
   const [searchBoxVisibility,setSearchBoxVisibility] = useState(false);
-  const handleSearch = (e) => {
+  useEffect(() => {
+    if (access_token) {
+      axios.get(process.env.REACT_APP_SERVER_DOMAIN + '/new-notification', {
+        headers: {
+          'Authorization': `Bearer ${access_token}`
+        }
+      })
+      .then((response) => {
+        //console.log(response.data); // Log the response to see its structure
+        setUserAuth({ ...userAuth, ...response.data }); // Update state based on response
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    }
+  }, [access_token]);
+
+  const handleBlogSearch = (e) => {
       let query = e.target.value;
       if(e.keyCode === 13){
           navigate(`/search/${query}`)
       }
   }
+  const handlePodcastSearch = (e) => {
+    let query = e.target.value;
+    if(e.keyCode === 13){
+        navigate(`podcast/search/${query}`)
+    }
+}
 
   let Links=[
     {name:"Home",link:"/"},
@@ -87,13 +110,13 @@ const Navbar = ({home=1,activeLink="Home"}) => {
               </div>
               <div className="sm:z-40 sm:fixed sm:right-0">
                 <ul className={`md:flex ${theme==="light"?"sm:bg-black":"sm:bg-white"} sm:pr-10 md:items-center sm:pt-10 sm:pb-20 sm:-mt-20 ${theme==="light" ? "text-white" : "text-black"} md:pb-0 pb:12 absolute md:static md:z-auto z-[-1] md:right-0 md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in sm:min-h-screen ${open ? "right-0":" right-[-200px]" }`} >
-                <div className={`${activeLink==="Blogs" ? "show" : "hidden"} flex items-center ml-auto gap-3 md:gap-6`}>
+                <div className={`${activeLink==="Blogs" || activeLink==="Podcasts" ? "show" : "hidden"} flex items-center ml-auto gap-3 md:gap-6`}>
                   <div className={`absolute left-0 w-full top-16 mt-0.5  px-[5vw] py-4 pb-1 border-b border-grey duration-500 md:-m-6 md:ml-2  md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show ${searchBoxVisibility ? "show" : "hide"}`}>
                   <input 
                       type='text'
                       placeholder='Search'
                       className='w-full md:w-auto bg-grey p-3 pl-6 pr-[12%] md:pr-6 rounded-full md:pl-12 text-black'
-                      onKeyDown={handleSearch}
+                      onKeyDown={activeLink==="Podcasts" ? handlePodcastSearch : handleBlogSearch}
                   />
                   <i className="fi fi-rr-search absolute right-[10%] top-1/2 sm:-translate-y-2 md:top-3 md:pointer-events-none md:left-5 text-lg text-dark-grey"></i>
                 </div>
@@ -123,6 +146,20 @@ const Navbar = ({home=1,activeLink="Home"}) => {
                   <Link>
                     <button onClick={changeTheme} className={`rounded-full w-11 h-11 sm:hidden hover:bg-black/10 ${home===1 ? "bg-black/20" : "bg-grey/90"} relative`}>
                             <i className={`fi fi-rr-${theme === "light" ? "moon-stars" :"brightness"} ${home===1? "text-white" : "text-black"} text-xl block mt-1`}></i>
+                    </button>
+                  </Link>
+                  </div>
+
+                  {/* Notifiactions */}
+                  <div className="ml-4">
+                  <Link to="dashboard/notifications">
+                    <button  className={`rounded-full w-11 h-11 hover:bg-black/10 ${home===1 ? "bg-black/20" : "bg-grey/90"} relative`}>
+                     
+                      <i className={`fi fi-rr-bell sidebar-icon text-xl block mt-1 ${home===1? "text-white" : "text-black"}`}></i>
+                      {
+                        new_notification_available ?<span className="bg-red w-3 h-3 rounded-full absolute z-10 top-2 right-2"></span>:""
+                      }
+                     
                     </button>
                   </Link>
                   </div>
