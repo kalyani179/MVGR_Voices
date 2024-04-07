ac8f6d1bca5fe9fd58c6454a691f153d00987d11
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-04-07 19:37:40 +0530

    modified index route

--- client/src/components/Podcast/api/index.js
+++ client/src/components/Podcast/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3001/";
+const baseURL = process.env.REACT_APP_SERVER_DOMAIN;
 
 
 // Assuming this is your existing code to fetch all podcasts in the frontend
