9cc84b4d32207ae3d39ea0b8ec9c58d60cb6b38d
Kalyani Dantuluri <dantulurikalyani999@gmail.com>
2024-04-15 23:50:02 +0530

    modified userAuth message to signup,signin

--- client/src/components/Authentication/UserAuth.jsx
+++ client/src/components/Authentication/UserAuth.jsx
@@ -105,7 +105,10 @@ const UserAuth = ({type,close,open}) => {
     const handleSubmit = (e) =>{
         e.preventDefault();
         if(type==="signup"){
-            return toast.error("Please continue with Google to sign up!");
+            return toast.error("Continue with Google to SignUp !");
+        }
+        if(type==="signin"){
+            return toast.error("Continue with Google to SignIn !");
         }
         let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
         let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
